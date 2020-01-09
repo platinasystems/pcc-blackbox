9fb8fd7e8700adb03c6e6a774b53021f8f6a213a
Stig Thormodsrud <stig@platinasystems.com>
2020-01-08 18:55:16 -0600

    configure invader interfaces also
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- configNetworkInterfaces.go
+++ configNetworkInterfaces.go
@@ -18,43 +18,73 @@ func configNetworkIntefaces(t *testing.T) {
 	test.SkipIfDryRun(t)
 	assert := test.Assert{t}
 	var (
-		err          error
-		ifaces       []*models.InterfaceDetail
-		iface        *models.InterfaceDetail
-		ifaceRequest models.InterfaceRequest
+		err    error
+		ifaces []*models.InterfaceDetail
 	)
 	for _, i := range Env.Servers {
 		ifaces, err = getIfacesByNodeId(NodebyHostIP[i.HostIp])
 		if err != nil {
-			assert.Fatalf("Error in retrieving node %v id[%v] interfaces", i.HostIp, NodebyHostIP[i.HostIp])
+			assert.Fatalf("Error retrieving node %v id[%v] "+
+				"interfaces", i.HostIp, NodebyHostIP[i.HostIp])
 		}
-		for j := 0; j < len(i.NetInterfaces); j++ {
-			fmt.Printf("Looking for %v MacAddress\n", i.NetInterfaces[j].MacAddr)
-			iface, err = getIfaceByMacAddress(i.NetInterfaces[j].MacAddr, ifaces)
-			if err != nil {
-				assert.Fatalf("Error in retrieving interface having MacAddress: %v for node %v id[%v]", i.NetInterfaces[j].MacAddr, i.HostIp, NodebyHostIP[i.HostIp])
-			}
-			if iface != nil {
-				ifaceRequest = models.InterfaceRequest{
-					InterfaceId:   iface.Interface.Id,
-					NodeId:        NodebyHostIP[i.HostIp],
-					Name:          iface.Interface.Name,
-					Ipv4Addresses: i.NetInterfaces[j].Cidrs,
-					MacAddress:    i.NetInterfaces[j].MacAddr,
-					ManagedByPcc:  i.NetInterfaces[j].ManagedByPcc,
-					Gateway:       i.NetInterfaces[j].Gateway,
-					Autoneg:       i.NetInterfaces[j].Autoneg,
-					Speed:         json.Number(i.NetInterfaces[j].Speed),
-					Mtu:           json.Number(i.NetInterfaces[j].Mtu),
-				}
-				if iface.Interface.IsManagement {
-					ifaceRequest.IsManagement = "true"
-				}
-				if setIface(ifaceRequest) != nil {
-					assert.Fatalf("Error in setting interface %v for node %v id[%v]", ifaceRequest, i.HostIp, NodebyHostIP[i.HostIp])
-				}
-			}
-		}
+		configNodeInterfaces(t, i.HostIp, i.NetInterfaces, ifaces)
+
 	}
-	time.Sleep(180 * time.Second)
+	for _, i := range Env.Invaders {
+		ifaces, err = getIfacesByNodeId(NodebyHostIP[i.HostIp])
+		if err != nil {
+			assert.Fatalf("Error retrieving node %v id[%v] "+
+				"interfaces", i.HostIp, NodebyHostIP[i.HostIp])
+		}
+		configNodeInterfaces(t, i.HostIp, i.NetInterfaces, ifaces)
+
+	}
+	time.Sleep(180 * time.Second) // lame
+}
+
+func configNodeInterfaces(t *testing.T, HostIp string,
+	serverInterfaces []netInterface, ifaces []*models.InterfaceDetail) {
+
+	assert := test.Assert{t}
+	var (
+		iface        *models.InterfaceDetail
+		ifaceRequest models.InterfaceRequest
+		err          error
+	)
+
+	for j := 0; j < len(serverInterfaces); j++ {
+		fmt.Printf("Looking for %v MacAddress\n",
+			serverInterfaces[j].MacAddr)
+		iface, err = getIfaceByMacAddress(serverInterfaces[j].MacAddr,
+			ifaces)
+		if err != nil {
+			assert.Fatalf("Error in retrieving interface having "+
+				"MacAddress: %v for node %v id[%v]",
+				serverInterfaces[j].MacAddr, HostIp,
+				NodebyHostIP[HostIp])
+		}
+		if iface == nil {
+			continue
+		}
+		ifaceRequest = models.InterfaceRequest{
+			InterfaceId:   iface.Interface.Id,
+			NodeId:        NodebyHostIP[HostIp],
+			Name:          iface.Interface.Name,
+			Ipv4Addresses: serverInterfaces[j].Cidrs,
+			MacAddress:    serverInterfaces[j].MacAddr,
+			ManagedByPcc:  serverInterfaces[j].ManagedByPcc,
+			Gateway:       serverInterfaces[j].Gateway,
+			Autoneg:       serverInterfaces[j].Autoneg,
+			Speed:         json.Number(serverInterfaces[j].Speed),
+			Mtu:           json.Number(serverInterfaces[j].Mtu),
+		}
+		if iface.Interface.IsManagement {
+			ifaceRequest.IsManagement = "true"
+		}
+		if setIface(ifaceRequest) == nil {
+			continue
+		}
+		assert.Fatalf("Error setting interface %v for node %v id[%v]",
+			ifaceRequest, HostIp, NodebyHostIP[HostIp])
+	}
 }
