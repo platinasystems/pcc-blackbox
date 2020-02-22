2dfa8e8e12f0df8421a8a65d1572a585cdab47c1
Stig Thormodsrud <stig@platinasystems.com>
2020-02-22 00:31:05 -0600

    portus needs some nodes
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- main_test.go
+++ main_test.go
@@ -156,7 +156,7 @@ func TestPortus(t *testing.T) {
 	fmt.Printf("Iteration %v, %v\n", count, time.Now().Format("Mon Jan 2 15:04:05 2006"))
 	mayRun(t, "portus", func(t *testing.T) {
 		mayRun(t, "getNodesList", getNodes)
-		//mayRun(t, "addBrownfieldNodes", addBrownfieldServers)
+		mayRun(t, "addBrownfieldNodes", addBrownfieldServers)
 		mayRun(t, "uploadSecurityAuthProfileCertificate", UploadSecurityAuthProfileCert)
 		mayRun(t, "addProfile", AddAuthenticationProfile)
 		mayRun(t, "uploadSecurityPortusKey", UploadSecurityPortusKey)
