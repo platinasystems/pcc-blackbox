9bc04719c0816add95b5d4f2b8813ade403d2d81
Stig Thormodsrud <stig@platinasystems.com>
2020-06-30 21:13:00 -0500

    Fix compile error due to changes in portus models in tiles
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>
    (cherry picked from commit 0aa4b23c69e77138c6088c9007661ea0fd469714)

--- portus.go
+++ portus.go
@@ -88,7 +88,7 @@ func installPortus(t *testing.T) {
 			if err != nil {
 				fmt.Printf("Get private key %s failed\n%v\n", PORTUS_KEY_FILENAME, err)
 			} else if exist {
-				portusConfiguration.RegistryKeyId = &privateKey.Id
+				portusConfiguration.RegistryCertId = &privateKey.Id
 			}
 
 			fmt.Printf("Installing Portus on Node with id %v\n",
