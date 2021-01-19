6b080e1120e42522d05bc74d55ea64bfe48d8562
smukherjee-platina <smukherjee@platinasystems.com>
2021-01-19 12:52:36 -0800

    PCC-3288: Add methods to test new Dashboard REST endpoint - Initial checkin

---
+++ lib/dashboard.go
@@ -0,0 +1,59 @@
+package pcc
+
+import (
+	"fmt"
+
+	dashboardctl "github.com/platinasystems/tiles/pccserver/controllers"
+	"github.com/platinasystems/tiles/pccserver/pccobject"
+)
+
+const subEndpoint string = "pccserver/dashboard"
+
+type DashboardObj struct {
+    dashboardctl.PccObjectOutput
+}
+
+type DashboardObjList struct {
+    dashboardctl.PccObjectOutputList
+}
+
+func (client *PccClient) TestDashboardObjectList(sortParam *string, pageParam *string) (*DashboardObjList, error) {
+    // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+
+    return nil, nil
+}
+
+func (client *PccClient) TestDashboardObjectById(id uint64) (*DashboardObj, error) {
+    return nil, nil
+}
+
+func (client *PccClient) TestDashboardFilteredObjectList(sortParam *string, pageParam *string) (*DashboardObjList, error) {
+    // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+
+    return nil, nil
+}
+
+func (client *PccClient) TestDashboardAdvSearchedObjectList(sortParam *string, pageParam *string) (*DashboardObjList, error) {
+    // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+
+    return nil, nil
+}
+
+func (client *PccClient) TestDashboardChildrenObjectList(id uint64, sortParam *string, pageParam *string) (*DashboardObjList, error) {
+    // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+
+    return nil, nil
+}
+
+func (client *PccClient) TestDashboardParentsObjectList(id uint64, sortParam *string, pageParam *string) (*DashboardObjList, error) {
+    // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+
+    return nil, nil
+}
+
+func (client *PccClient) TestDashboardAggrHealthCountByType() (*pccobject.AggrHealthCountByType, error) {
+
+    return nil, nil
+}
+
+
