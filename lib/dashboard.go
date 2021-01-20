c31a1ba118e3a7ede75a62a8db58d034195515f3
smukherjee-platina <smukherjee@platinasystems.com>
2021-01-19 17:09:32 -0800

    PCC-3288: Add more test func for exercising Dashboard API

--- lib/dashboard.go
+++ lib/dashboard.go
@@ -1,29 +1,26 @@
 package pcc
 
 import (
-	 "fmt"
+     "fmt"
 
-	dashboardctl "github.com/platinasystems/tiles/pccserver/controllers"
-	"github.com/platinasystems/tiles/pccserver/pccobject"
+    dashboardctl "github.com/platinasystems/tiles/pccserver/controllers"
+    "github.com/platinasystems/tiles/pccserver/pccobject"
 )
 
+// Are these wrapper types helpful? CHECK LATER...
+// >>>>>>
 type DashboardObj struct {
-    dashboardctl.PccObjectOutput	`json:"PccObjectOutput"`
+    dashboardctl.PccObjectOutput    `json:"PccObjectOutput"`
 }
 
-/*
-type DashboardObjList struct {
-    dashboardctl.PccObjectOutputList
-}*/
 type DashboardObjList []DashboardObj
+// <<<<<<
 
-func (client *PccClient) TestDashboardObjectList(sortParam *string, pageParam *string) (objects *[]dashboardctl.PccObjectOutput, e error) {
+func (client *PccClient) TestDashboardObjectList(sortParam *string, pageParam *string) (objects *[]dashboardctl.PccObjectOutput, err error) {
     // sort & pagination params may not be specified, i.e. the params are either nil or empty string
-    var (
-		result []dashboardctl.PccObjectOutput = make ([]dashboardctl.PccObjectOutput, 0)
-		err error
-	)
     endPoint := "pccserver/dashboard/objects"
+    var result []dashboardctl.PccObjectOutput = make([]dashboardctl.PccObjectOutput, 0)
+
     if sortParam != nil {
         endPoint = endPoint + "?" + endPoint
     }
@@ -31,18 +28,18 @@ func (client *PccClient) TestDashboardObjectList(sortParam *string, pageParam *s
         endPoint = endPoint + "&" + endPoint
     }
     if err = client.Get(endPoint, &result); err == nil {
-		objects = &result
-	} else {
-	    objects = nil
-	}
+        objects = &result
+    } else {
+        objects = nil
+    }
     return objects, err
 }
 
-func (client *PccClient) TestDashboardObjectById(id uint64) (obj *dashboardctl.PccObjectOutput, e error) {
-	endPoint := fmt.Sprintf("pccserver/dashboard/objects/%d", id);
+func (client *PccClient) TestDashboardObjectById(id uint64) (obj *dashboardctl.PccObjectOutput, err error) {
+    endPoint := fmt.Sprintf("pccserver/dashboard/objects/%d", id)
     var result dashboardctl.PccObjectOutput
-	var err error
-	if err = client.Get(endPoint, &result); err == nil {
+
+    if err = client.Get(endPoint, &result); err == nil {
         obj = &result
     } else {
         obj = nil
@@ -50,32 +47,102 @@ func (client *PccClient) TestDashboardObjectById(id uint64) (obj *dashboardctl.P
     return obj, err
 }
 
-func (client *PccClient) TestDashboardFilteredObjectList(sortParam *string, pageParam *string) (*DashboardObjList, error) {
+func (client *PccClient) TestDashboardFilteredObjectList(field string, value string, sortParam *string, pageParam *string) (objects *[]dashboardctl.PccObjectOutput, err error) {
     // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+    endPoint := fmt.Sprintf("pccserver/dashboard/objects/%s/%s", field, value)
+    var result []dashboardctl.PccObjectOutput = make([]dashboardctl.PccObjectOutput, 0)
 
-    return nil, nil
+    if sortParam != nil {
+        endPoint = endPoint + "?" + endPoint
+    }
+    if pageParam != nil {
+        endPoint = endPoint + "&" + endPoint
+    }
+    if err = client.Get(endPoint, &result); err == nil {
+        objects = &result
+    } else {
+        objects = nil
+    }
+    return objects, err
 }
 
-func (client *PccClient) TestDashboardAdvSearchedObjectList(sortParam *string, pageParam *string) (*DashboardObjList, error) {
+func (client *PccClient) TestDashboardAdvSearchedObjectList(filter string, sortParam *string, pageParam *string) (objects *[]dashboardctl.PccObjectOutput, err error) {
     // sort & pagination params may not be specified, i.e. the params are either nil or empty string 
-    return nil, nil
+    endPoint := fmt.Sprintf("pccserver/dashboard/objects/search?filter=%s", filter)
+    var result []dashboardctl.PccObjectOutput = make([]dashboardctl.PccObjectOutput, 0)
+
+    if sortParam != nil {
+        endPoint = endPoint + "&" + endPoint
+    }
+    if pageParam != nil {
+        endPoint = endPoint + "&" + endPoint
+    }
+    if err = client.Get(endPoint, &result); err == nil {
+        objects = &result
+    } else {
+        objects = nil
+    }
+    return objects, err
 }
 
-func (client *PccClient) TestDashboardChildrenObjectList(id uint64, sortParam *string, pageParam *string) (*DashboardObjList, error) {
+func (client *PccClient) TestDashboardChildrenObjectList(id uint64, sortParam *string, pageParam *string) (objects *[]dashboardctl.PccObjectOutput, err error) {
     // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+    endPoint := fmt.Sprintf("pccserver/dashboard/objects/childrenOf/%d", id)
+    var result []dashboardctl.PccObjectOutput = make([]dashboardctl.PccObjectOutput, 0)
 
-    return nil, nil
+    if sortParam != nil {
+        endPoint = endPoint + "?" + endPoint
+    }
+    if pageParam != nil {
+        endPoint = endPoint + "&" + endPoint
+    }
+    if err = client.Get(endPoint, &result); err == nil {
+        objects = &result
+    } else {
+        objects = nil
+    }
+    return objects, err
 }
 
-func (client *PccClient) TestDashboardParentsObjectList(id uint64, sortParam *string, pageParam *string) (*DashboardObjList, error) {
+func (client *PccClient) TestDashboardParentsObjectList(id uint64, sortParam *string, pageParam *string) (objects *[]dashboardctl.PccObjectOutput, err error) {
     // sort & pagination params may not be specified, i.e. the params are either nil or empty string
+    endPoint := fmt.Sprintf("pccserver/dashboard/objects/parentsOf/%d", id)
+    var result []dashboardctl.PccObjectOutput = make([]dashboardctl.PccObjectOutput, 0)
 
-    return nil, nil
+    if sortParam != nil {
+        endPoint = endPoint + "?" + endPoint
+    }
+    if pageParam != nil {
+        endPoint = endPoint + "&" + endPoint
+    }
+    if err = client.Get(endPoint, &result); err == nil {
+        objects = &result
+    } else {
+        objects = nil
+    }
+    return objects, err
 }
 
-func (client *PccClient) TestDashboardAggrHealthCountByType() (*pccobject.AggrHealthCountByType, error) {
+func (client *PccClient) TestDashboardAggrHealthCountByType() (healthCount *pccobject.AggrHealthCountByType, err error) {
+    endPoint := fmt.Sprintf("pccserver/dashboard/stats/health/countByType")
+    var result pccobject.AggrHealthCountByType
 
-    return nil, nil
+    if err = client.Get(endPoint, &result); err == nil {
+        healthCount = &result
+    } else {
+        healthCount = nil
+    }
+    return healthCount, err
 }
 
+func (client *PccClient) TestDashboardMetadataEnumStrings() (metadataStrings *dashboardctl.DashboardStringsList, err error) {
+    endPoint := fmt.Sprintf("pccserver/dashboard/metadata/enum/strings")
+    var result dashboardctl.DashboardStringsList
 
+    if err = client.Get(endPoint, &result); err == nil {
+        metadataStrings = &result
+    } else {
+        metadataStrings = nil
+    }
+    return metadataStrings, err
+}
--- testDashboard.go
+++ testDashboard.go
@@ -1,32 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"testing"
-_	"time"
-_	"github.com/platinasystems/pcc-blackbox/lib"
+    "fmt"
+    "math/rand"
+    "testing"
+    "time"
 )
 
 // get whole PCC ObjectsList
 func testDashboardGetAllPCCObjects(t *testing.T) {
-	fmt.Println("Get whole PCCObjects list with no sort or pagination")
-	pccObjects, err := Pcc.TestDashboardObjectList(nil, nil)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("Received # [%d] objects\n", len(*pccObjects))
-	}
-	checkError(t, err)
+    fmt.Println("Get whole PCCObjects list with no sort or pagination")
+    pccObjects, err := Pcc.TestDashboardObjectList(nil, nil)
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Printf("Received [%d] PCC Objects\n", len(*pccObjects))
+    }
+    checkError(t, err)
 }
 
-func testDashboardGetPCCObjectById (t *testing.T) {
-    fmt.Println("Get PCCObject by Id")
-	pccObject, err := Pcc.TestDashboardObjectById(4)
-	_ = pccObject
-	if err != nil {
-       fmt.Println(err)  
-	} else {
-        fmt.Printf("Received # [%d] objects\n", 1)
+type IdList []uint64
+
+func testDashboardGetPCCObjectByRandomId(t *testing.T) {
+    fmt.Println("Get PCCObject by Random Id")
+    pccObjects, err := Pcc.TestDashboardObjectList(nil, nil)
+    idList := make(IdList, 0)
+    if err == nil {
+        for _, obj := range *pccObjects {
+            idList = append(idList, obj.Id)
+        }
+        rand.Seed(time.Now().UnixNano())
+        id := rand.Uint64() % uint64(len(idList))
+        pccObject, err := Pcc.TestDashboardObjectById(id)
+        if err == nil {
+            fmt.Printf("Received PCC Object with id=[%d]\n", pccObject.Id)
+        }
     }
-    checkError(t, err)	
+
+    checkError(t, err)
+}
+
+func testDashboardGetPCCObjectById(t *testing.T) {
+    fmt.Println("Get PCCObject by Id")
+    pccObject, err := Pcc.TestDashboardObjectById(4)
+    _ = pccObject
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println("Received 1 PCC Object")
+    }
+    checkError(t, err)
 }
