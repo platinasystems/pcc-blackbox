b1e57fd2df08851b68d3cec083a4231dbd83e5ba
Gianluca Arena <arena.gianluca@outlook.it>
2021-12-06 16:51:51 +0100

    Add OSD down check

--- testAlerts.go
+++ testAlerts.go
@@ -10,8 +10,11 @@ import (
 	log "github.com/platinasystems/go-common/logs"
 	pcc "github.com/platinasystems/pcc-blackbox/lib"
 	m "github.com/platinasystems/pcc-blackbox/models"
+	"github.com/platinasystems/pcc-models/notification"
 	"github.com/platinasystems/test"
 	"github.com/platinasystems/tiles/pccserver/models"
+	//"github.com/platinasystems/pcc-models/notification"
+	monitorModels "github.com/platinasystems/platina-monitor/models"
 	"io"
 	"io/ioutil"
 	"strings"
@@ -22,67 +25,183 @@ import (
 const (
 	HighUsageAlertName     = "ceph pools high usage"
 	VeryHighUsageAlertName = "ceph pools very high usage"
+	OSDDownOut             = "osds down/out"
 )
 
-var oldMail string
-var poolName string
+var (
+	oldMail            string
+	poolName           string
+	pool1, pool2       string
+	osdServer, osdHost string
+	osdID              int
+	savedNotifications map[string][]monitorModels.RuleNotification
+)
 
-func testPoolUsageAlert(t *testing.T) {
-	t.Run("changeRootMail", changeRootMail)
+func test2PoolUsageAlert(t *testing.T) {
+	t.Run("testChangeMailAlerts", testChangeMailAlerts)
 
 	t.Run("createPool", createPool)
 	t.Run("verifyPool", verifyPool)
+	pool1 = poolName
+	t.Run("createPool", createPool)
+	t.Run("verifyPool", verifyPool)
+	pool2 = poolName
 
-	log.AuctaLogger.Info("Waiting 1m")
-	time.Sleep(1 * time.Minute)
+	waitMinutes(1 * time.Minute)
 
+	usePool1()
+	t.Run("testEmptyPoolPrometheus", testEmptyPoolPrometheus)
+	t.Run("addFile85MiB", addFile85MiB)
+	usePool2()
 	t.Run("testEmptyPoolPrometheus", testEmptyPoolPrometheus)
 	t.Run("addFile85MiB", addFile85MiB)
 
-	log.AuctaLogger.Info("Waiting for the alert to fire, 3m")
-	time.Sleep(3 * time.Minute)
+	waitMinutes(3 * time.Minute)
 
+	usePool1()
+	t.Run("testPoolOver80Prometheus", testPoolOver80Prometheus)
+	t.Run("testMonitorRuleFiring80", testMonitorRuleFiring80)
+	t.Run("testNotificationRuleFiring80", testNotificationRuleFiring80)
+	t.Run("testMailFiring80", testMailFiring80)
+	t.Run("addFile10MiB", addFile10MiB)
+	usePool2()
 	t.Run("testPoolOver80Prometheus", testPoolOver80Prometheus)
 	t.Run("testMonitorRuleFiring80", testMonitorRuleFiring80)
 	t.Run("testNotificationRuleFiring80", testNotificationRuleFiring80)
 	t.Run("testMailFiring80", testMailFiring80)
 	t.Run("addFile10MiB", addFile10MiB)
 
-	log.AuctaLogger.Info("Waiting for the alert to fire, 3m")
-	time.Sleep(3 * time.Minute)
+	waitMinutes(3 * time.Minute)
 
+	usePool1()
 	t.Run("testPoolOver90Prometheus", testPoolOver90Prometheus)
 	t.Run("testMonitorRuleFiring90", testMonitorRuleFiring90)
 	t.Run("testNotificationRuleFiring90", testNotificationRuleFiring90)
 	t.Run("testMailFiring90", testMailFiring90)
+	t.Run("delAllFiles", delAllFiles)
 
-	t.Run("createPool", createPool)
-	t.Run("verifyPool", verifyPool)
+	usePool2()
+	t.Run("testPoolOver90Prometheus", testPoolOver90Prometheus)
+	t.Run("testMonitorRuleFiring90", testMonitorRuleFiring90)
+	t.Run("testNotificationRuleFiring90", testNotificationRuleFiring90)
+	t.Run("testMailFiring90", testMailFiring90)
+	t.Run("delAllFiles", delAllFiles)
 
-	log.AuctaLogger.Info("Waiting 1m")
-	time.Sleep(1 * time.Minute)
+	waitMinutes(3 * time.Minute)
 
+	usePool1()
 	t.Run("testEmptyPoolPrometheus", testEmptyPoolPrometheus)
-	t.Run("addFile85MiB", addFile85MiB)
+	t.Run("testMonitorRuleResolved80", testMonitorRuleResolved80)
+	t.Run("testMonitorRuleResolved90", testMonitorRuleResolved90)
+	t.Run("testNotificationRuleResolved80", testNotificationRuleResolved80)
+	t.Run("testNotificationRuleResolved90", testNotificationRuleResolved90)
+	t.Run("testMailResolved80", testMailResolved80)
+	t.Run("testMailResolved90", testMailResolved90)
+	t.Run("removePool", removePool)
 
-	log.AuctaLogger.Info("Waiting for the alert to fire, 3m")
-	time.Sleep(3 * time.Minute)
+	usePool2()
+	t.Run("testEmptyPoolPrometheus", testEmptyPoolPrometheus)
+	t.Run("testMonitorRuleResolved80", testMonitorRuleResolved80)
+	t.Run("testMonitorRuleResolved90", testMonitorRuleResolved90)
+	t.Run("testNotificationRuleResolved80", testNotificationRuleResolved80)
+	t.Run("testNotificationRuleResolved90", testNotificationRuleResolved90)
+	t.Run("testMailResolved80", testMailResolved80)
+	t.Run("testMailResolved90", testMailResolved90)
+	t.Run("removePool", removePool)
 
-	t.Run("testPoolOver80Prometheus", testPoolOver80Prometheus)
-	t.Run("testMonitorRuleFiring80", testMonitorRuleFiring80)
-	t.Run("testNotificationRuleFiring80", testNotificationRuleFiring80)
-	t.Run("testMailFiring80", testMailFiring80)
-	t.Run("addFile10MiB", addFile10MiB)
+	t.Run("restoreMailAlerts", restoreMailAlerts)
+}
 
-	log.AuctaLogger.Info("Waiting for the alert to fire, 3m")
-	time.Sleep(3 * time.Minute)
+func testOSDDownAlert(t *testing.T) {
+	t.Run("testChangeMailAlerts", testChangeMailAlerts)
 
-	t.Run("testPoolOver90Prometheus", testPoolOver90Prometheus)
-	t.Run("testMonitorRuleFiring90", testMonitorRuleFiring90)
-	t.Run("testNotificationRuleFiring90", testNotificationRuleFiring90)
-	t.Run("testMailFiring90", testMailFiring90)
+	t.Run("testOSDDown", testOSDDown)
 
-	t.Run("resetRootMail", resetRootMail)
+	waitMinutes(10 * time.Minute)
+
+	t.Run("testOSDDownPrometheus", testOSDDownPrometheus)
+	t.Run("testMonitorRuleFiringOSD", testMonitorRuleFiringOSD)
+	t.Run("testNotificationRuleFiringOSD", testNotificationRuleFiringOSD)
+	t.Run("testMailFiringOSD", testMailFiringOSD)
+
+	t.Run("testOSDUp", testOSDUp)
+
+	waitMinutes(3 * time.Minute)
+
+	t.Run("testOSDUpPrometheus", testOSDUpPrometheus)
+	t.Run("testMonitorRuleResolvedOSD", testMonitorRuleResolvedOSD)
+	t.Run("testNotificationRuleResolvedOSD", testNotificationRuleResolvedOSD)
+	t.Run("testMailResolvedOSD", testMailResolvedOSD)
+
+	t.Run("restoreMailAlerts", restoreMailAlerts)
+}
+
+func testChangeMailAlerts(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	savedNotifications = make(map[string][]monitorModels.RuleNotification)
+
+	for _, name := range []string{HighUsageAlertName, VeryHighUsageAlertName, OSDDownOut} {
+		rule, err := Pcc.GetRuleByName(name)
+		checkError(t, res, err)
+
+		savedNotifications[name] = rule.Notifications
+
+		notification := monitorModels.RuleNotification{
+			NotificationService: notification.NotificationService{
+				Service: "email",
+				Inputs: []notification.NotificationInput{
+					{
+						Name:  "to",
+						Value: "aucta.tenant@gmail.com",
+					},
+				},
+			},
+		}
+
+		rule.Notifications = []monitorModels.RuleNotification{notification}
+
+		_, err = Pcc.UpdateRule(rule, rule.Id)
+		checkError(t, res, err)
+	}
+}
+
+func restoreMailAlerts(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	for _, name := range []string{HighUsageAlertName, VeryHighUsageAlertName, OSDDownOut} {
+		rule, err := Pcc.GetRuleByName(name)
+		checkError(t, res, err)
+
+		notifications := savedNotifications[name]
+
+		for n := range notifications {
+			notifications[n].Id = 0
+		}
+		rule.Notifications = notifications
+
+		_, err = Pcc.UpdateRule(rule, rule.Id)
+		checkError(t, res, err)
+	}
+}
+
+func usePool1() {
+	poolName = pool1
+}
+
+func usePool2() {
+	poolName = pool2
+}
+
+func waitMinutes(minutes time.Duration) {
+	log.AuctaLogger.Info(fmt.Sprintf("Waiting %d m", minutes/time.Minute))
+	time.Sleep(minutes)
 }
 
 func changeRootMail(t *testing.T) {
@@ -191,6 +310,53 @@ func verifyPool(t *testing.T) {
 			}
 		}
 	}
+}
+
+func removePool(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	cephCluster, err := Pcc.GetCephCluster(Env.AlertsConfiguration.ClusterName)
+	checkError(t, res, err)
+
+	pool, err := Pcc.GetCephPool(poolName, cephCluster.Id)
+	err = Pcc.DeleteCephPool(pool.Id)
+	checkError(t, res, err)
+}
+
+func testOSDDown(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	cluster, err := Pcc.GetCephCluster(Env.AlertsConfiguration.ClusterName)
+	checkError(t, res, err)
+	osds, err := Pcc.GetOSDsStateByClusterID(cluster.Id)
+	checkError(t, res, err)
+	targetOSD := osds[0]
+
+	osdNode, err := Pcc.GetNodeByName(targetOSD.Server)
+	checkError(t, res, err)
+
+	osdHost = osdNode.Host
+	osdServer = targetOSD.Server
+	osdID = targetOSD.Osd
+
+	_, _, err = Pcc.SSHHandler().Run(osdHost, fmt.Sprintf("sudo systemctl stop ceph-osd@%d.service", osdID))
+	checkError(t, res, err)
+}
+
+func testOSDUp(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	_, _, err := Pcc.SSHHandler().Run(osdHost, fmt.Sprintf("sudo systemctl start ceph-osd@%d.service", osdID))
+	checkError(t, res, err)
 }
 
 func addFiles(t *testing.T, res *m.TestResult, poolName string, fileSize ...string) {
@@ -315,14 +481,54 @@ func testPoolOver90Prometheus(t *testing.T) {
 	checkError(t, res, err)
 }
 
-func testMonitorRule(t *testing.T, res *m.TestResult, ruleName string, objName string) {
+func checkDownOSDsNumPrometheus(t *testing.T, res *m.TestResult) int {
+	query := "numOsdsNotUp"
+
+	result, err := Pcc.InstantQuery(query)
+	checkError(t, res, err)
+
+	log.AuctaLogger.Infof("result: %v", result.Value)
+	return int(result.Value)
+}
+
+func testOSDDownPrometheus(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	downOSDs := checkDownOSDsNumPrometheus(t, res)
+
+	var err error
+	if downOSDs == 0 {
+		err = errors.New("There should be at least an OSD down/out")
+	}
+	checkError(t, res, err)
+}
+
+func testOSDUpPrometheus(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	downOSDs := checkDownOSDsNumPrometheus(t, res)
+
+	var err error
+	if downOSDs > 0 {
+		err = errors.New("All OSDs should be up")
+	}
+	checkError(t, res, err)
+}
+
+func testMonitorRuleFiring(t *testing.T, res *m.TestResult, ruleName string, objName string) {
 	rules, err := Pcc.GetRules()
 	checkError(t, res, err)
 
 	for _, r := range *rules {
 		if r.Name == ruleName {
 			for _, a := range r.Alerts {
-				if a.ObjName == objName {
+				if a.ObjName == objName || objName == "" {
 					return
 				}
 			}
@@ -339,7 +545,7 @@ func testMonitorRuleFiring80(t *testing.T) {
 	res := m.InitTestResult(runID)
 	defer res.CheckTestAndSave(t, time.Now())
 
-	testMonitorRule(t, res, HighUsageAlertName, poolName)
+	testMonitorRuleFiring(t, res, HighUsageAlertName, poolName)
 }
 
 func testMonitorRuleFiring90(t *testing.T) {
@@ -348,7 +554,61 @@ func testMonitorRuleFiring90(t *testing.T) {
 	res := m.InitTestResult(runID)
 	defer res.CheckTestAndSave(t, time.Now())
 
-	testMonitorRule(t, res, VeryHighUsageAlertName, poolName)
+	testMonitorRuleFiring(t, res, VeryHighUsageAlertName, poolName)
+}
+
+func testMonitorRuleFiringOSD(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMonitorRuleFiring(t, res, OSDDownOut, osdServer)
+}
+
+func testMonitorRuleResolved(t *testing.T, res *m.TestResult, ruleName string, objName string) {
+	rules, err := Pcc.GetRules()
+	checkError(t, res, err)
+
+	for _, r := range *rules {
+		if r.Name == ruleName {
+			for _, a := range r.Alerts {
+				if a.ObjName == objName || objName == "" {
+					err = errors.New("Alert still active in monitoring")
+					checkError(t, res, err)
+					return
+				}
+			}
+		}
+	}
+	return
+}
+
+func testMonitorRuleResolved80(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMonitorRuleResolved(t, res, HighUsageAlertName, poolName)
+}
+
+func testMonitorRuleResolved90(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMonitorRuleResolved(t, res, VeryHighUsageAlertName, poolName)
+}
+
+func testMonitorRuleResolvedOSD(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMonitorRuleResolved(t, res, OSDDownOut, osdServer)
 }
 
 func testNotifications(t *testing.T, res *m.TestResult, status string, alert string, objName string) {
@@ -385,6 +645,42 @@ func testNotificationRuleFiring90(t *testing.T) {
 	testNotifications(t, res, "firing", "90%", poolName)
 }
 
+func testNotificationRuleFiringOSD(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testNotifications(t, res, "firing", "OSDs", osdServer)
+}
+
+func testNotificationRuleResolved80(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testNotifications(t, res, "resolved", "80%", poolName)
+}
+
+func testNotificationRuleResolved90(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testNotifications(t, res, "resolved", "90%", poolName)
+}
+
+func testNotificationRuleResolvedOSD(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testNotifications(t, res, "resolved", "OSDs", osdServer)
+}
+
 func testMailKeywords(t *testing.T, res *m.TestResult, keywords ...string) {
 	c, err := client.DialTLS("imap.gmail.com:993", nil)
 	checkError(t, res, err)
@@ -400,9 +696,9 @@ func testMailKeywords(t *testing.T, res *m.TestResult, keywords ...string) {
 	// Get the last 4 messages
 	from := uint32(1)
 	to := mbox.Messages
-	if mbox.Messages > 3 {
+	if mbox.Messages > 10 {
 		// We're using unsigned integers here, only subtract if the result is > 0
-		from = mbox.Messages - 3
+		from = mbox.Messages - 10
 	}
 	seqset := new(imap.SeqSet)
 	seqset.AddRange(from, to)
@@ -434,15 +730,13 @@ func testMailKeywords(t *testing.T, res *m.TestResult, keywords ...string) {
 			case *mail.InlineHeader:
 				// This is the message's text (can be plain-text or HTML)
 				b, _ := ioutil.ReadAll(p.Body)
-				split := strings.Split(string(b), "<dt>Rule:</dt>")
-				for _, s := range split {
-					ok := false
-					for _, k := range keywords {
-						ok = ok && strings.Contains(s, k)
-					}
-					if ok {
-						return
-					}
+				body := string(b)
+				ok := true
+				for _, k := range keywords {
+					ok = ok && strings.Contains(body, k)
+				}
+				if ok {
+					return
 				}
 
 			case *mail.AttachmentHeader:
@@ -473,4 +767,40 @@ func testMailFiring90(t *testing.T) {
 	defer res.CheckTestAndSave(t, time.Now())
 
 	testMailKeywords(t, res, "90%", "firing", poolName)
+}
+
+func testMailFiringOSD(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMailKeywords(t, res, "OSDs", "firing", osdServer)
+}
+
+func testMailResolved80(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMailKeywords(t, res, "80%", "resolved", poolName)
+}
+
+func testMailResolved90(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMailKeywords(t, res, "90%", "resolved", poolName)
+}
+
+func testMailResolvedOSD(t *testing.T) {
+	test.SkipIfDryRun(t)
+
+	res := m.InitTestResult(runID)
+	defer res.CheckTestAndSave(t, time.Now())
+
+	testMailKeywords(t, res, "OSDs", "resolved", osdServer)
 }
